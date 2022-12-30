a20aaf7344eb80d60aa97c7e10b731033d7b0302
Lee Hyungmok <hyungmok@hyungmok.local>
2022-12-30 16:44:37 +0900

    Add filtering and versioning

--- src/main/java/com/hyungmok/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/hyungmok/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.hyungmok.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +12,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message="Name should have at least 2 characters.")
+	@JsonProperty("userName")
 	private String name;
 	
 	@Past(message="Birth date should be in the past.")
+	@JsonProperty
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
